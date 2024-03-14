11cebc8df98f25a753f18d5c1c28251937a53e95
unknown <abdullah.mamun.0110@gmail.com>
2024-03-14 23:55:39 +0600

    bio updated

--- src/components/Profile/Bio.jsx
+++ src/components/Profile/Bio.jsx
@@ -46,6 +46,9 @@ const Bio = ({ bio,isMe }) => {
                 <button onClick={handleBio} type="submit" className="inline-flex items-center px-5 py-2.5 my-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
                     update bio
                 </button>
+                <button onClick={closeModal} type="submit" className="inline-flex mx-3 items-center px-5 py-2.5 my-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                    close
+                </button>
             </div>
 
             }
